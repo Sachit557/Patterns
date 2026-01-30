052159d23f6bf8f9ff0a51e7d65bf1d8725eec19
Sachit Gupta <sachitgu11@gmail.com>
2026-01-30 11:42:53 +0530

    Refactor code structure for improved readability and maintainability

--- Patterns_cpp/Hollow_diamond_pattern.cpp
+++ Patterns_cpp/Hollow_diamond_pattern.cpp
@@ -13,14 +13,26 @@ int main(void)
 void hollow_diamond_pattern(int a)
 {
     int spaces = a - 1;
-    for (int i = 0; i <= a; i++)
+    for (int i = 0; i < a; i++)
     {
         for (int j = 0; j < spaces; j++)
         {
-            std::cout << " " << "hello";
+            std::cout << " ";
         }
-        spaces--;
 
+        std::cout << '*';
+
+        for (int k = 0; k < 2 * i - 1; k++)
+            std::cout << " ";
+        if (i != 0)
+            std::cout << "*";
+
+        // end part
+        spaces--;
         std::cout << std::endl;
     }
+
+    int b = a - 1;
+
+    // rest loop here ( second loop)
 }
